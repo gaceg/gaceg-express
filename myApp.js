18bf2cbc0e30c6de42f8f0e5b8d823e51c0e4be1
Marcin <marcin@logdev.pl>
2022-06-26 14:11:16 +0200

    Update

--- myApp.js
+++ myApp.js
@@ -1,39 +1,19 @@
 var express = require('express');
 var app = express();
+require("dotenv").config();
+console.log("Hello World");
 
+app.get("/json",(req,res) => {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  let response = "Hello json"
+  if(process.env.MESSAGE_STYLE === 'uppercase'){
+      res.json({ message:response.toUpperCase() })
+  }
+  else{
+    
+    res.json({ message:response })
+  }
+  
+});
 
  module.exports = app;
